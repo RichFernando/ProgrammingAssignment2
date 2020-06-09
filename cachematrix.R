135b0b0b9c0f2f03d460e2ba5a7368c550ee7bb9
RichFernando <richard.a.f.fernando@gmail.com>
2020-06-10 10:41:24 +1200

    Matrix now has inverse taken
    Undertakes the inverse matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,28 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## invM formed as a NULL value so that it can be called up
+## set creates x and invM as variables outside of this envrionment
 
+## the 
+
+makeCacheMatrix <- function(x = matrix()) {
+        
+        invM <- NULL
+        set <- function(y) {
+                x <<- y
+                invM <<- NULL
+        }
+        
+        ## creating functions that call up 
+        get <- function() x                             ## pulls up x
+        setinvM <- function(solve) invM <<- solve       ## sets invM to call UP the 
+                                                        ## solve function
+        getinvM <- function() invM                      ## runs the solve function
+        list(set = set, get = get,
+             setinvM = setinvM,
+             getinvM = getinvM)
+        ## list generates these functions as the output of the function                         
 }
 
 
@@ -12,4 +32,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        invM <- x$getinvM()
+        if(!is.null(invM)) {
+                message("getting cached data")
+                return(invM)
+        }
+        data <- x$get()
+        invM <- solve(data, ...)
+        x$setinvM(invM)
+        invM
+
+        
 }
